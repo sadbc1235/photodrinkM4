a3c904f9acd76f16259185f7f3f779af9236f054
sadbc1235 <sadbc1235@naver.com>
2022-08-01 23:37:27 +0900

    remake photodrinkM3

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 
 const boothBoxs = document.querySelectorAll(".boothBox");
 const conceptName = {
-  neon: ["Sweet Peach", "Mango Green", "Mixed Blue", "Shadow Pink"],
-  profile: ["Soft-Right", "Soft-Left", "Contrast-Right", "Contrast-Left"],
+  neon: ["Sweet Peach", "Mango Green", "Mixed<br> Blue", "Shadow Pink"],
+  profile: ["Soft-Right", "Soft-Left", "Contrast Right", "Contrast Left"],
   modern: ["Soft", "Contrast", "Sepia", "Blue"],
   art: ["Heart", "Night Sky", "Butterfly", "Full Moon"],
   dynamic: ["Photo", "Video"]
@@ -61,326 +61,142 @@ const modernFilter = [
   "rgba(255,201,90,.2)", // sepia
   "rgba(75,158,219,.3)" // blue
 ]
+const cardBtn = document.querySelector(".cardBtn .btnName");
+const cards = document.querySelectorAll(".card");
 
-let disable = true;
-let backBtnDisable = true;
+const clickMore = (e) => {
+  const card = e.path[3];
+  const cardClose = card.querySelector(".cardClose");
+  const btnBox = e.target.parentNode;
+  const conBtnBox = btnBox.querySelector(".conBtnBox");
+  const conBtns = conBtnBox.querySelectorAll(".conBtn");
+  const btnName = e.target;
+  const cardExplain = e.target.nextElementSibling;
 
-const loading = document.querySelector(".loading");
-
-
-for(let i=0; i<boothBoxs.length; i++) {
-  let intoConceptBtn = boothBoxs[i].querySelector(".intoConceptBtn");
-  let boothInner = boothBoxs[i].querySelector(".boothInner");
-  let boothName = boothBoxs[i].querySelector(".boothName");
-  let backBtn = boothName.querySelector(".backBtn");
-  let boothExplain = boothBoxs[i].querySelector(".boothExplain");
-  let conceptListBox = boothBoxs[i].querySelector(".conceptListBox");
-  let conceptItems = conceptListBox.querySelectorAll(".conceptItem");
-
-
-  // more button click event
-  intoConceptBtn.addEventListener("click", () => {
-
-    if(disable) {
-      disable = false;
-
-      for(let j=0; j<boothBoxs.length; j++) {
-        if (i !== j) {
-          boothBoxs[j].classList.add("fadeout");
-        }
-      }
-      
-      setTimeout(() => {
-
-        boothBoxs[i].style.display = "block";
-        conceptListBox.style.display = "block";
-        boothBoxs[i].classList.add("active");
-        boothInner.classList.add("active");
-
-        for(let j=0; j<boothBoxs.length; j++) {
-          if (i !== j) {
-            boothBoxs[j].style.display = "none";
-          }
-        }
-
-        setTimeout(() => {
-          backBtn.style.display = "block";
-          boothExplain.style.display = "block";
-
-        }, 300)
-  
-        setTimeout(() => {
-          conceptListBox.style.opacity = 1;
-          backBtn.style.opacity = 1;
-          boothExplain.style.opacity = 1;
-          
-          for(let i=0; i<conceptItems.length; i++) {
-            conceptItems[i].style.transform = "translateY(-150%)";
-          }
-
-        }, 380)
-  
-      }, 350)
-    }
-
+  cards.forEach(card => {
+    card.classList.add("fadeOut");
   })
 
-  // concept button click event
-  let boothImg = boothBoxs[i].querySelector(".boothImg");
-  let mainImg = boothImg.querySelector(".mainImg");
-  let filters = boothImg.querySelectorAll(".filter");
-  let conceptImg = boothImg.querySelector(".conceptImgBox").querySelector("img");
-  let filterListBox = intoConceptBtn.querySelector(".filterListBox");
-  let filterNames = filterListBox.querySelectorAll(".filterName");
-  let circles = filterListBox.querySelectorAll(".circle");
+  card.classList.remove("fadeOut");
+  setTimeout(() => {
+    card.classList.add("showCon");
+    btnName.classList.remove("showName");
 
-  for(let j=0; j<conceptItems.length; j++) {
-    
+    setTimeout(() => {
+      cardExplain.classList.add("showEx");
 
-    conceptItems[j].addEventListener("click", () => {
-      document.querySelector("video").pause();
-      document.querySelector("video").currentTime = 0;
-
-      boothExplain.style.opacity = 0;
-      mainImg.style.opacity = 0;
-      for (let j=0; j<filters.length; j++) {
-        filters[j].classList.remove("active");
-        filters[j].classList.remove("active2");
-        filterNames[j].classList.remove("active");
-      }
-
-      for(let i=0; i<conceptItems.length; i++) {
-        conceptItems[i].style.transitionDelay = "0s";
-      }
-
-      conceptItems.forEach( conceptItem => {
-        conceptItem.classList.remove("active");
-      })
-      conceptItems[j].classList.add("active");
-
-      conceptImg.style.opacity = 0;
-      filterListBox.style.opacity = 0;
-      backBtn.style.opacity = 0;
-      loading.classList.add("active");
-      backBtnDisable = false;
-
-      if(conceptItems[j].innerText.includes("NEON")) {
+      setTimeout(() => {
+        conBtnBox.classList.add("showBtnBox");
+        conBtns[0].classList.add("showConBtn");
 
         setTimeout(() => {
-
-          switch(boothName.querySelector("h2").innerText) {
-            case "Bust Shot":
-              conceptImg.setAttribute("src", bustConceptImgSrc.neon);
-            break;
-            case "Metal":
-              conceptImg.setAttribute("src", metalConceptImgSrc.neon);
-            break;
-            case "Full Shot":
-              conceptImg.setAttribute("src", fullConceptImgSrc.neon);
-            break;
-          }
-
-            for(let i=0; i<neonFilter.length; i++) {
-            
-              filters[i].style.background = neonFilter[i];
-              circles[i].style.background = neonFilter[i];
-              filterNames[i].innerText = conceptName.neon[i];
-            }
-
-        }, 200)
-        
-      } else if(conceptItems[j].innerText.includes("Profile")) {
-
-        setTimeout(() => {
-
-          switch(boothName.querySelector("h2").innerText) {
-            case "Bust Shot":
-              conceptImg.setAttribute("src", bustConceptImgSrc.profile);
-            break;
-            case "Metal":
-              conceptImg.setAttribute("src", metalConceptImgSrc.profile);
-            break;
-            case "Full Shot":
-              conceptImg.setAttribute("src", fullConceptImgSrc.profile);
-            break;
-          }
-          
-
-          for(let i=0; i<neonFilter.length; i++) {
-            filters[i].style.background = profileFilter[i];
-            circles[i].style.background = profileFilter[i];
-            filterNames[i].innerText = conceptName.profile[i];
-          }
-        }, 200)
-        
-      } else if(conceptItems[j].innerText.includes("Modern")) {
-
-        setTimeout(() => {
-
-          switch(boothName.querySelector("h2").innerText) {
-            case "Bust Shot":
-              conceptImg.setAttribute("src", bustConceptImgSrc.modern);
-            break;
-            case "Metal":
-              conceptImg.setAttribute("src", metalConceptImgSrc.modern);
-            break;
-            case "Full Shot":
-              conceptImg.setAttribute("src", fullConceptImgSrc.modern);
-            break;
-          }
-          
-
-          for(let i=0; i<neonFilter.length; i++) {
-            filters[i].style.background = modernFilter[i];
-            circles[i].style.background = modernFilter[i];
-            filterNames[i].innerText = conceptName.modern[i];
-          }
-        }, 200)
-        
-      } else if(conceptItems[j].innerText.includes("Art")) {
-
-        setTimeout(() => {
-          conceptImg.setAttribute("src", fullConceptImgSrc.art);
-
-          for(let i=0; i<neonFilter.length; i++) {
-            filters[i].style = artFilter[i];
-            circles[i].style = artBtnImg[i];
-            filterNames[i].innerText = conceptName.art[i];
-          }
-          filters[0].classList.add("active2");
-          filterNames[0].classList.add("active");
-        }, 200)
-        
-      } else if(conceptItems[j].innerText.includes("Dynamic")) {
-
-        setTimeout(() => {
-          conceptImg.setAttribute("src", dynamicConceptImgSrc);
-          filters[0].style = dynamicFilter;
-          for(let i=0; i<conceptName.dynamic.length; i++) {
-            circles[i].style = dynamicBtnImg[i];
-            filterNames[i].innerText = conceptName.dynamic[i];
-          }
-        }, 200)
-      }
-
-      conceptImg.addEventListener("load", () => {
-        filterListBox.style.opacity = 1;
-        conceptImg.style.opacity = 1;
-        backBtn.style.opacity = 1;
-        backBtnDisable = true;
-        boothImg.classList.add("active");
-        loading.classList.remove("active");
-      })
-      
-    })
-
-  }
-
-  let filterItems = filterListBox.querySelectorAll(".filterItem");
-
-  for(let i=0; i<filterItems.length; i++) {
-    filterItems[i].addEventListener("click", () => {
-
-      
-      for (let j=0; j<filters.length; j++) {
-        filters[j].classList.remove("active2");
-        filters[j].classList.remove("active");
-        filterNames[j].classList.remove("active");
-      }
-
-        if(filterNames[i].innerText == "Heart" ||
-           filterNames[i].innerText == "Night Sky" ||
-           filterNames[i].innerText == "Butterfly" ||
-           filterNames[i].innerText == "Full Moon" ||
-           filterNames[i].innerText == "Photo" ||
-           filterNames[i].innerText == "Video") {
-
-          filters[i].classList.add("active2");
-          filterNames[i].classList.add("active");
-          if (filterNames[i].innerText == "Video") {
-            document.querySelector("video").play();
-          } else {
-            document.querySelector("video").pause();
-            document.querySelector("video").currentTime = 0;
-          }
-
-        } else {
-          filters[i].classList.add("active");
-          filterNames[i].classList.add("active");
-        }
-      
-    })
-  }
-
-    // back button click event
-    if(backBtnDisable) {
-      backBtn.addEventListener("click", () => {
-        document.querySelector("video").pause();
-        document.querySelector("video").currentTime = 0;
-        boothExplain.style.opacity = 0;
-        conceptImg.style.opacity = 0;
-  
-        for (let j=0; j<filters.length; j++) {
-          filters[j].classList.remove("active");
-          filters[j].classList.remove("active2");
-          filterNames[j].classList.remove("active");
-        }
-  
-        conceptItems.forEach( conceptItem => {
-          conceptItem.classList.remove("active");
-        })
-        conceptImg.style.opacity = 0;
-        filterListBox.style.opacity = 0;
-        
-  
-        let delay = 0;
-        for(let i=0; i<conceptItems.length; i++) {
-          conceptItems[i].style.transitionDelay = delay+"s";
-          delay += 0.2;
-        }
-    
-        
-        conceptListBox.style.opacity = 0;
-        backBtn.style.opacity = 0;
-        
-        setTimeout(() => {
-          mainImg.style.opacity = 1;
-          boothImg.classList.remove("active");
-          
-          for(let i=0; i<conceptItems.length; i++) {
-            conceptItems[i].style.transform = "translateY(0%)";
-          }
-          backBtn.style.display = "none";
-    
-        }, 300)
-    
-        setTimeout(() => {
-          boothBoxs[i].style.display = "flex";
-          conceptListBox.style.display = "none";
-          boothBoxs[i].classList.remove("active");
-          boothInner.classList.remove("active");
-    
-          for(let j=0; j<boothBoxs.length; j++) {
-            if (i !== j) {
-              boothBoxs[j].style.display = "flex";
-            }
-          }
+          conBtns[1].classList.add("showConBtn");
     
           setTimeout(() => {
-          
-            for(let j=0; j<boothBoxs.length; j++) {
-              if (i !== j) {
-                boothBoxs[j].classList.remove("fadeout");
-              }
+            conBtns[2].classList.add("showConBtn");
+            
+            if(conBtns.length == 4) {
+              setTimeout(() => {
+                conBtns[3].classList.add("showConBtn");
+              }, 200)
             }
-            disable = true;
-    
-          }, 380)
-        }, 320)
-    
-      })
+
+            setTimeout(() => {
+              cardClose.classList.add("showClose");
+            }, 400)
+          }, 200)
+        }, 200)
+      }, 400)
+    }, 450)
+  }, 400)
+  
+}
+
+const clickConBtn = (e) => {
+  const card = e.path[4];
+  const conBtnBox = card.querySelector(".conBtnBox");
+  const mainImg = card.querySelector(".mainImg");
+  const cardExplain = card.querySelector(".cardExplain");
+  const conBox = card.querySelector(".conBox");
+  const conNames = conBox.querySelectorAll(".conName");
+  const conCircle = conBox.querySelectorAll(".conCircle");
+  const imgFilter = card.querySelectorAll(".imgFilter");
+
+  cardExplain.classList.remove("showEx");
+  conBox.classList.remove("showConBox");
+  mainImg.classList.add("closeImg");
+
+  for(let i=0; i<imgFilter.length; i++) {
+    imgFilter[i].classList.remove("showFilter");
+  }
+
+  let boothSrc;
+  if(card.className.includes("bust")) {
+
+    if(card.className.includes("bust")) {
+      boothSrc = bustConceptImgSrc;
+    } else if(card.className.includes("metal")) {
+      boothSrc = metalConceptImgSrc;
+    } else if(card.className.includes("profile")) {
+      boothSrc = fullConceptImgSrc;
+    } else if(card.className.includes("dyna")) {
+      boothSrc = dynamicConceptImgSrc;
     }
 
-    
+    let currentConBtn = conBtnBox.querySelector(".active");
+
+    setTimeout(() => {
+      switch(currentConBtn.innerText) {
+        case "NEON":
+          mainImg.setAttribute("src", boothSrc.neon);
+          for(let i=0; i<conNames.length; i++) {
+            conNames[i].innerHTML = conceptName.neon[i];
+            conCircle[i].style.background = neonFilter[i];
+            imgFilter[i].style.background = neonFilter[i];
+          }
+        break;
+        case "Profile":
+          mainImg.setAttribute("src", boothSrc.profile);
+          for(let i=0; i<conNames.length; i++) {
+            conNames[i].innerHTML = conceptName.profile[i];
+            conCircle[i].style.background = profileFilter[i];
+            imgFilter[i].style.background = profileFilter[i];
+          }
+        break;
+        case "Modern Black":
+          mainImg.setAttribute("src", boothSrc.modern);
+          for(let i=0; i<conNames.length; i++) {
+            conNames[i].innerHTML = conceptName.modern[i];
+            conCircle[i].style.background = modernFilter[i];
+            imgFilter[i].style.background = modernFilter[i];
+          }
+        break;
+      }
+      
+      mainImg.addEventListener("load", () => {
+        conBox.classList.add("showConBox");
+        mainImg.classList.remove("closeImg");
+      })
+    }, 400)
+  }
 
 }
+
+cardBtn.addEventListener("click", (e) => {
+  clickMore(e);
+})
+
+cards.forEach(card => {
+  const conBtnBox = card.querySelector(".conBtnBox");
+  const conBtns = conBtnBox.querySelectorAll(".conBtn");
+
+  conBtns.forEach(conBtn => {
+    conBtn.addEventListener("click", (e) => {
+      for(let i=0; i<conBtns.length; i++) {
+        conBtns[i].classList.remove("active");
+      }
+      conBtn.classList.add("active");
+      clickConBtn(e);
+    })
+  })
+})
